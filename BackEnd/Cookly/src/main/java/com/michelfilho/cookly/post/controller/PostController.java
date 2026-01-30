f99b84ffe020f2d9ae48e53bb48c4864b2dcb8b9
Michel Filho <michelhbfilho@gmail.com>
2026-01-30 16:41:20 -0300

    PostController documented

--- BackEnd/Cookly/src/main/java/com/michelfilho/cookly/post/controller/PostController.java
+++ BackEnd/Cookly/src/main/java/com/michelfilho/cookly/post/controller/PostController.java
@@ -6,6 +6,10 @@ import com.michelfilho.cookly.post.dto.ReadPostDTO;
 import com.michelfilho.cookly.post.model.Post;
 import com.michelfilho.cookly.post.service.InteractPostService;
 import com.michelfilho.cookly.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +32,12 @@ public class PostController {
     @Autowired
     private InteractPostService interactPostService;
 
+
+    @Operation(summary = "Publish a post", description = "Given all post information and images it publishes.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Post successfully published"),
+            @ApiResponse(responseCode = "400", description = "Can't publish the post")
+    })
     @PostMapping(
             value = "/new",
             consumes = MediaType.MULTIPART_FORM_DATA_VALUE
@@ -40,35 +50,54 @@ public class PostController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Given an ID, it try to delete a post.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Post successfully deleted."),
+            @ApiResponse(responseCode = "404", description = "Post not found"),
+            @ApiResponse(responseCode = "403", description = "This user can't delete this post.")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity delete(@PathVariable String id, @AuthenticationPrincipal UserDetails user) {
+    public ResponseEntity delete(@PathVariable @Parameter(example = "UUID") String id, @AuthenticationPrincipal UserDetails user) {
         postService.removePost(id, user);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Given an username, return all posts of this person paginated.")
     @GetMapping("/{username}")
     public List<ReadPostDTO> getAllByUsername(@PathVariable String username, @RequestParam(value = "page", required = false, defaultValue = "1") Integer page) {
         return postService.findPostsByUsername(username, page);
     }
 
+    @Operation(summary = "Given an post ID try to like the post with the logged user.")
+    @ApiResponses(value =  {
+            @ApiResponse(responseCode = "200", description = "Successfully liked"),
+            @ApiResponse(responseCode = "400", description = "Post already liked by the user.")
+    })
     @PostMapping("/{id}/like")
-    public ResponseEntity likePost(@PathVariable String id, @AuthenticationPrincipal UserDetails user) {
+    public ResponseEntity likePost(@PathVariable @Parameter(example = "UUID") String id, @AuthenticationPrincipal UserDetails user) {
         interactPostService.like(user, id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Given an post ID try to dislike the post with the logged user.")
+    @ApiResponses(value =  {
+            @ApiResponse(responseCode = "200", description = "Successfully disliked"),
+            @ApiResponse(responseCode = "400", description = "Post isn't liked by the user.")
+    })
     @DeleteMapping("/{id}/like")
     public ResponseEntity dislikePost(@PathVariable String id, @AuthenticationPrincipal UserDetails user) {
         interactPostService.dislike(user, id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Given an post ID comment on it with the logged user.")
     @PostMapping("/{id}/comment")
-    public ResponseEntity comment(@PathVariable String id, @AuthenticationPrincipal UserDetails user, @RequestBody String content) {
+    public ResponseEntity comment(@PathVariable @Parameter(example = "UUID") String id, @AuthenticationPrincipal UserDetails user, @RequestBody @Parameter(example = "Very good recipe!") String content) {
         interactPostService.comment(user, id, content);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Given an comment id try to delete it.")
     @DeleteMapping("/comment/{commentId}")
     public ResponseEntity removeComment(
             @AuthenticationPrincipal UserDetails user,
--- BackEnd/Cookly/src/main/java/com/michelfilho/cookly/post/dto/NewPostDTO.java
+++ BackEnd/Cookly/src/main/java/com/michelfilho/cookly/post/dto/NewPostDTO.java
@@ -1,5 +1,6 @@
 package com.michelfilho.cookly.post.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,12 +11,16 @@ import java.util.List;
 
 public record NewPostDTO(
         @NotBlank
+        @Schema(example = "Love Apple")
         String recipeName,
         @Size(max=150)
+        @Schema(example = "A sugared apple")
         String description,
         @Min(1)
+        @Schema(example = "15")
         Integer prepareTime,
         @Size(min=1, max=15)
+        @Schema(example = "[\"turn on the oven\", \"turn off the oven\"]")
         List<String> stepsToPrepare
 ) {
 }
